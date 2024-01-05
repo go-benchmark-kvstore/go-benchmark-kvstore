ad7a6ea6dff16cfe3e7688f14159ffa247254b5f
Mitar <mitar.git@tnode.com>
2024-01-05 08:38:42 +0100

    Improve logging.

--- logger.go
+++ logger.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/rs/zerolog"
 )
 
@@ -12,22 +15,36 @@ func (loggerWrapper) Close() error {
 	return nil
 }
 
+func (l loggerWrapper) log(level zerolog.Level, msg string, args ...interface{}) {
+	if len(args) > 0 {
+		l.Logger.WithLevel(level).Msgf(strings.TrimRight(msg, "\n"), args...)
+		return
+	}
+	lines := strings.Split(msg, "\n")
+	for _, line := range lines {
+		if len(line) > 0 {
+			l.Logger.WithLevel(level).Msg(line)
+		}
+	}
+}
+
 func (l loggerWrapper) Debugf(msg string, args ...interface{}) {
-	l.Logger.Debug().Msgf(msg, args...)
+	l.log(zerolog.DebugLevel, msg, args...)
 }
 
 func (l loggerWrapper) Errorf(msg string, args ...interface{}) {
-	l.Logger.Error().Msgf(msg, args...)
+	l.log(zerolog.ErrorLevel, msg, args...)
 }
 
 func (l loggerWrapper) Infof(msg string, args ...interface{}) {
-	l.Logger.Info().Msgf(msg, args...)
+	l.log(zerolog.InfoLevel, msg, args...)
 }
 
 func (l loggerWrapper) Warningf(msg string, args ...interface{}) {
-	l.Logger.Warn().Msgf(msg, args...)
+	l.log(zerolog.WarnLevel, msg, args...)
 }
 
 func (l loggerWrapper) Fatalf(msg string, args ...interface{}) {
-	l.Logger.Fatal().Msgf(msg, args...)
+	l.log(zerolog.FatalLevel, msg, args...)
+	os.Exit(1)
 }
